b2aa5302947e26ab09b3fbb2dbb01028b6341037
Bob <q19900106@gmail.com>
2018-03-08 13:08:22 +0800

    fix run error

--- djangosite/djangosite/settings.py
+++ djangosite/djangosite/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y0#e-e#y^t148om^z0$)$^%b7%zif%ln^uh-7ils&ma$r0k-ie'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -34,7 +35,7 @@ ALLOWED_HOSTS = [
 
 
 # Application definition
-
+#'csai',
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'trips',
     'uploader',
-    'csai',
 ]
 
 MIDDLEWARE = [
